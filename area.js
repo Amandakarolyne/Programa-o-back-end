79d9e44f7edb0d2d976e9fb1ef4f8c0fe52275b9
Amandakarolyne <151687486+Amandakarolyne@users.noreply.github.com>
2026-05-27 10:37:21 -0300

    Add files via upload

---
+++ area.js
@@ -0,0 +1,5 @@
+let comprimento = Number (prompt("Informe o comprimento: "));
+let largura = Number (prompt("Informe a largura: "));
+let area = comprimento + largura
+alert(area)
+document.write('A area é ${area}')
