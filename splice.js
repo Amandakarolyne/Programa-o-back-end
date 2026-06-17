454143b4e23ed4cc60a4deb26181de3c40b00a1e
Amandakarolyne <151687486+Amandakarolyne@users.noreply.github.com>
2026-06-17 11:39:00 -0300

    Add files via upload

---
+++ splice.js
@@ -0,0 +1,20 @@
+//let fila = ["Ana", "Beto", "Carlos","Diana"];
+//fila.splice(1,1, "Edu", "Fábio")
+//console.log(fila)
+
+//let pontos = [150, 132, 110, 95, 80, 75, 60];
+//let top3 = pontos.slice(0,3)
+//console.log(top3)
+
+
+//let dados = [10, 20,  "ERRO", "ERRO", 50, 60];
+//dados.splice(2,2)
+//console.log(dados)
+
+//let convidados = ["Leo", "Bia", "Caio", "Lara", "Noah"];
+//let ultimos = convidados.slice(-2)
+//console.log(Ultimos);
+
+//let cores =["Vermelho", "Verde", "Azul"];
+//cores.splice(1,1, "Amarelo")
+//console.log(cores)
