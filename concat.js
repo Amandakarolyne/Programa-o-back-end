f51f3c3dac170b6c92ccb00c3f833b90e54d072b
Amandakarolyne <151687486+Amandakarolyne@users.noreply.github.com>
2026-08-12 13:26:12 -0300

    Add concat.js to demonstrate array concatenation

---
+++ concat.js
@@ -0,0 +1,25 @@
+let frutas1 = ["maçã", "banana"];
+let frutas2 = ["uva", "laranja"];
+let frutas = frutas1.concat(frutas2);
+console.log(frutas);
+
+let numeros1 = [1, 2, 3];
+let numeros2 = [4, 5, 6];
+let numeros = numeros1.concat(numeros2);
+console.log(numeros);
+
+let manhã = ["Matemática", "Português"];
+let tarde = ["História", "Geografia"];
+let noite = ["Inglês"];
+let materias = manhã.concat(tarde, noite);
+console.log(materias);
+
+let nomes = ["Ana", "Carlos"];
+let sobrenomes = ["Silva", "Souza"];
+let pessoas = nomes.concat(sobrenomes);
+console.log(pessoas);
+
+clet lista1 = ["mouse", "teclado"];
+let lista2 = ["monitor", "gabinete"];
+let produtos = lista1.concat(lista2);
+console.log(produtos);
